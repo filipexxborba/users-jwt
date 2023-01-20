ee5b2038b0223d7744c33c43a4e62efa9b39d135
Filipe Borba <48929981+filipexxborba@users.noreply.github.com>
2023-01-20 16:41:09 -0300

    feat: Swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Users JWT and Bcrypt authentication')
+    .setDescription('Users JWT and Bcrypt authentication project')
+    .setVersion('1.0.0')
+    .addTag('User', 'user')
+    .build();
+
+  // Swagger initialization
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
