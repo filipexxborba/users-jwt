cd6906d434a46b421f2ec01b7c675b392975b1e2
Filipe Borba <48929981+filipexxborba@users.noreply.github.com>
2023-01-20 15:50:27 -0300

    fix: Fixing some path broke

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Request, Post, Get, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from 'auth/jwt-auth.guard';
+import { LocalAuthGuard } from 'auth/guards/local-auth.guard';
+import { JwtAuthGuard } from 'auth/guards/jwt-auth.guard';
 
 @Controller()
 export class AppController {
